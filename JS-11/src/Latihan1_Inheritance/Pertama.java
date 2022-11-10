11bbdb1e68d99fa1a707893a24b14b63f1543979
21343010-Natasyafebriani <112762485+21343010-Natasyafebriani@users.noreply.github.com>
2022-11-10 21:33:31 +0700

    Add files via upload

---
+++ JS-11/src/Latihan1_Inheritance/Pertama.java
@@ -0,0 +1,18 @@
+/*
+ * Created by: 21343010-Natasya ferbiani
+ */
+
+package Latihan1_Inheritance;
+
+public class Pertama {
+	private int a = 10;
+	
+	protected void terprotek() {
+		System.out.println("Method in hanya untuk anaknya");
+	}
+
+	public void info() {
+		System.out.println("a = " +a);
+	}
+}
+
