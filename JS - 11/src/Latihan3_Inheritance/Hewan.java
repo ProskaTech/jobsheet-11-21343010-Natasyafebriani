06d32e14d534b13b0655ab9773399f3edf20b38b
21343010-Natasyafebriani <112762485+21343010-Natasyafebriani@users.noreply.github.com>
2022-11-10 19:54:14 +0700

    Add files via upload

---
+++ JS - 11/src/Latihan3_Inheritance/Hewan.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ * @Created by: 21343011 - Nur Chintia Ningsih
+ */
+package Latihan3_Inheritance;
+
+public class Hewan {
+	public static void testClassMethod(){
+		System.out.println("The Class Method in Hewan");
+	}
+	
+	public void testInstanceMethod() {
+		System.out.println("The Instance Method in Hewan");
+		
+
+	}
+
+}
